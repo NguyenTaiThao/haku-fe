16fcf92fe4c79bb0d39203cce614f690704514c4
nguyentaithao <thaogreenhouses@gmail.com>
2022-11-21 18:59:25 +0700

    feat: card

---
+++ src/containers/Learn/Card.tsx
@@ -0,0 +1,40 @@
+// @ts-nocheck
+import React, { useRef, useState } from "react";
+import Flippy, { FrontSide, BackSide } from "react-flippy";
+
+export default function Card() {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  const ref = useRef();
+
+  return (
+    <div>
+      <Flippy
+        flipOnHover={false} // default false
+        flipOnClick={true} // default false
+        flipDirection="horizontal" // horizontal or vertical
+        ref={ref} // to use toggle method like ref.curret.toggle()
+        // if you pass isFlipped prop component will be controlled component.
+        // and other props, which will go to div
+        style={{ width: "200px", height: "200px" }} /// these are optional style, it is not necessary
+      >
+        <FrontSide style={{ backgroundColor: "#41669d" }}>
+          RICK <br />
+          <button
+            onClick={() => {
+              ref?.current?.toggle();
+            }}
+          >
+            {" "}
+            Toggle via button
+          </button>
+        </FrontSide>
+        <BackSide style={{ backgroundColor: "#175852" }}>ROCKS</BackSide>
+      </Flippy>
+    </div>
+  );
+}
--- src/containers/Learn/index.tsx
+++ src/containers/Learn/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Card from "./Card";
 
 export default function Learn() {
   return (
     <div>
       <h1>Learn</h1>
+      <Card />
     </div>
   );
 }
